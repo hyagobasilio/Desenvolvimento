b6c446b1f8464c20ac48150342a8917d2871c230
Jaderson Morais <jaderson.morais@gmail.com>
2014-06-05 22:44:05 -0300

    Ajuste na configuração e Criação de uma classe para teste WS

--- src/main/java/com/entity/Pessoa.java
+++ src/main/java/com/entity/Pessoa.java
@@ -22,6 +22,7 @@ public class Pessoa implements Serializable{
     private String email;
     private String relatorio;
     
+    public Pessoa(){}
 
 	@Id
     @GeneratedValue
@@ -79,5 +80,31 @@ public class Pessoa implements Serializable{
 	public void setRelatorio(String relatorio) {
 		this.relatorio = relatorio;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pessoa other = (Pessoa) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
     
 }
